3c90c5fd6b7e26043c8c8fd2c620a2a435eb8caf
Jacob MacDonald <jakemac@google.com>
2015-08-07 09:47:54 -0700

    iron-image test

---
+++ test/iron_image_test.dart
@@ -0,0 +1,63 @@
+@TestOn('browser')
+library polymer_elements.test.iron_image;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:js';
+import 'dart:math';
+import 'package:polymer_elements/iron_image.dart';
+import 'package:test/test.dart';
+import 'package:web_components/web_components.dart';
+import 'common.dart';
+
+var rand = new Random();
+
+main() async {
+  await initWebComponents();
+  
+  group('<iron-image>', () {
+    randomImageUrl () {
+      return 'fixtures/polymer.svg?' + rand.nextDouble().toString();
+    }
+
+    IronImage image;
+    
+    group('basic behavior', () {
+      setUp(() {
+        image = fixture('TrivialImage');
+      });
+
+      test('can load images given a src', () {
+        var done = new Completer();
+        image.on['loaded-changed'].take(1).listen((_) {
+          expect(image.loaded, isTrue);
+          done.complete();
+        });
+        image.src = randomImageUrl();
+        return done.future;
+      });
+
+      test('will reload images when src changes', () {
+        var loadCount = 0;
+        var done = new Completer();
+        StreamSubscription listener;
+        listener = image.on['loaded-changed'].listen((_) {
+          if (image.loaded) {
+            loadCount++;
+            if (loadCount == 2) {
+              listener.cancel();
+              done.complete();
+            } else {
+              wait(1).then((_) {
+                image.src = randomImageUrl();
+              });
+            }
+          }
+        });
+        image.src = randomImageUrl();
+        return done.future;
+      });
+    });
+  });
+}
