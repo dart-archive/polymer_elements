fcbea8767fd0a2c48a8129c14c8dcf028c99148a
Jacob MacDonald <jakemac@google.com>
2015-08-10 14:45:20 -0700

    google-map-markers add/remove test

---
+++ test/google_map_markers_add_remove_test.dart
@@ -0,0 +1,49 @@
+@TestOn('browser')
+library polymer_elements.test.google_map_markers_add_remove_test;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:js';
+import 'package:polymer_elements/google_map.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'package:test/test.dart';
+import 'package:web_components/web_components.dart';
+import 'common.dart';
+
+main() async {
+  await initWebComponents();
+
+  GoogleMap map = document.querySelector('#map1');
+
+  group('markers', () {
+    test('markers are defined, added, removed', () {
+      var done = new Completer();
+      map.on['google-map-ready'].take(1).listen((e) {
+        // Check if marker children were setup and can be added/removed.
+        // TODO(jakemac): https://github.com/dart-lang/polymer_elements/issues/20
+        expect(map.jsElement['markers'].length, 2);
+
+        // TODO(jakemac): https://github.com/dart-lang/polymer_elements/issues/20
+        var marker = map.jsElement['markers'][0];
+        Polymer.dom((Polymer.dom(marker).parentNode)).removeChild(marker);
+        PolymerDom.flush();
+        wait(1).then((_) {
+          // needed because map.updateMarkers has mutationObserver
+          // TODO(jakemac): https://github.com/dart-lang/polymer_elements/issues/20
+          expect(map.jsElement['markers'].length, 1);
+          expect(marker.marker['map'], isNull,
+              reason: 'removed marker is still visible on map');
+          Polymer.dom(map).append(marker);
+          PolymerDom.flush();
+          wait(1).then((_) {
+            expect(marker.marker['map'], isNotNull,
+                reason: 're-added marker is not visible.');
+            done.complete();
+          });
+        });
+      });
+      return done.future;
+    });
+  });
+}
