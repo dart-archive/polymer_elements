693f755848ef6067b653e85e8356d50a3a8f93da
Victor Sheyanov <victor.sheyanov@gmail.com>
2015-08-12 13:46:27 +0300

    golden elements tests

---
+++ test/gold_cc_expiration_input_test.dart
@@ -0,0 +1,94 @@
+@TestOn('browser')
+library polymer_elements.test.gold_cc_expiration_input;
+
+import 'package:polymer_elements/gold_cc_expiration_input.dart';
+import 'package:polymer_elements/date_input.dart';
+import 'package:web_components/web_components.dart';
+import 'package:test/test.dart';
+import 'common.dart';
+
+main() async {
+  await initWebComponents();
+
+  group('basic', () {
+    test('invalid input is not ok', () {
+      GoldCcExpirationInput input = fixture('basic');
+      input.value = '1234';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
+    });
+
+    test('misformed dates are not ok', () {
+      GoldCcExpirationInput input = fixture('basic');
+      input.value = '33/33';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
+    });
+
+    test('past dates are not ok', () {
+      GoldCcExpirationInput input = fixture('basic');
+      input.value = '11/00';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
+    });
+
+    test('future dates are ok', () {
+      GoldCcExpirationInput input = fixture('basic');
+      // Note: this test will start failing in 2099. Apologies, future maintainers.
+      input.value = '11/99';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isFalse);
+      expect(input.value, equals('11/99'));
+    });
+
+    test('value is updated correctly ', () {
+      GoldCcExpirationInput input = fixture('basic');
+
+      (input.querySelector('.paper-input-input') as DateInput).month = '11';
+      (input.querySelector('.paper-input-input') as DateInput).year = '15';
+      expect(input.value, equals('11/15'));
+    });
+
+    test('empty required input shows error', () {
+      GoldCcExpirationInput input = fixture('basic');
+      forceXIfStamp(input);
+
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error should not be display:none');
+    });
+  });
+
+  group('a11y', () {
+    test('has aria-labelledby', () {
+      GoldCcExpirationInput input = fixture('basic');
+      var month = input.querySelectorAll('input')[0];
+      var year = input.querySelectorAll('input')[1];
+      var label = input.querySelector('label').id;
+
+      expect(month, isNotNull);
+      expect(year, isNotNull);
+
+      expect(month.getAttribute('aria-labelledby'), isNotNull);
+      expect(year.getAttribute('aria-labelledby'), isNotNull);
+
+      expect(
+          month.getAttribute('aria-labelledby'), equals(label + ' monthLabel'));
+      expect(
+          year.getAttribute('aria-labelledby'), equals(label + ' yearLabel'));
+    });
+  });
+}
---
+++ test/gold_cc_input_test.dart
@@ -0,0 +1,98 @@
+@TestOn('browser')
+library polymer_elements.test.gold_cc_input;
+
+import 'package:polymer_elements/gold_cc_input.dart';
+import 'package:polymer_elements/iron_input.dart';
+import 'package:web_components/web_components.dart';
+import 'package:test/test.dart';
+import 'common.dart';
+
+main() async {
+  await initWebComponents();
+
+  group('basic', () {
+    test('input is spaced out correctly', () {
+      GoldCcInput input = fixture('basic');
+      input.value = '12345678';
+      expect(input.value, equals('1234 5678'));
+    });
+
+    test('invalid input is not ok', () {
+      GoldCcInput input = fixture('required');
+      input.value = '1234';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isTrue);
+      expect(input.cardType, equals(''));
+    });
+
+    test('valid input is ok', () {
+      GoldCcInput input = fixture('required');
+      input.value = '4000000000000002';
+      forceXIfStamp(input);
+
+      var container = input.querySelector('paper-input-container');
+      expect(container, isNotNull);
+      expect(container.invalid, isFalse);
+      expect(input.cardType, equals('visa'));
+    });
+
+    test('empty required input shows error', () {
+      GoldCcInput input = fixture('required');
+      forceXIfStamp(input);
+
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error should not be display:none');
+    });
+
+    test('invalid input shows error message after manual validation', () {
+      GoldCcInput input = fixture('ErrorWithoutAutoValidate');
+      forceXIfStamp(input);
+
+      var error = input.querySelector('paper-input-error');
+      expect(error, isNotNull);
+
+      // The error message is only displayed after manual validation.
+      expect(error.getComputedStyle().display, equals('none'),
+          reason: 'error should be display:none');
+      input.validate();
+      expect(error.getComputedStyle().display, isNot('none'),
+          reason: 'error should not be display:none');
+    }, skip: 'https://github.com/dart-lang/polymer_elements/issues/3');
+
+    test('caret position is preserved', () {
+      GoldCcInput input = fixture('required');
+      IronInput ironInput = input.querySelector('input[is="iron-input"]');
+      input.value = '1111 1111';
+      ironInput.selectionStart = 2;
+      ironInput.selectionEnd = 2;
+      input.jsElement.callMethod('_computeValue', ['1122 1111 11']);
+
+      expect(ironInput.selectionStart, equals(2),
+          reason: 'selectionStart should be preserved');
+      expect(ironInput.selectionEnd, equals(2),
+          reason: 'selectionEnd should be preserved');
+    });
+  });
+
+  group('a11y', () {
+    test('has aria-labelledby', () {
+      GoldCcInput input = fixture('basic');
+      expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
+      expect(input.inputElement.getAttribute('aria-labelledby'),
+          input.querySelector('label').id,
+          reason: 'aria-labelledby points to the label');
+    });
+    test('required and error has aria-labelledby', () {
+      GoldCcInput input = fixture('required');
+      expect(input.inputElement.getAttribute('aria-labelledby'), isNotNull);
+      expect(input.inputElement.getAttribute('aria-labelledby'),
+          input.querySelector('label').id,
+          reason: 'aria-labelledby points to the label');
+    });
+  });
+}
