78377ec059daebc6a61db1ca9196f6fec3cb2aea
Jacob MacDonald <jakemac@google.com>
2015-08-14 07:33:26 -0700

    add missing icons

---
+++ lib/image_icons.dart
@@ -0,0 +1,12 @@
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `image_icons`.
+@HtmlImport('image_icons_nodart.html')
+library polymer_elements.lib.src.iron_icons.image_icons;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'iron_icon.dart';
+import 'iron_iconset_svg.dart';
---
+++ lib/notification_icons.dart
@@ -0,0 +1,12 @@
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `notification_icons`.
+@HtmlImport('notification_icons_nodart.html')
+library polymer_elements.lib.src.iron_icons.notification_icons;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'iron_icon.dart';
+import 'iron_iconset_svg.dart';
