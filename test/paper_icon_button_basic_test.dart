ff9f7bd840d8991ce1a320cc5d2928c37987f66e
Jacob MacDonald <jakemac@google.com>
2015-08-19 09:09:12 -0700

    paper-icon-button basic test no longer skipped

--- test/paper_icon_button_basic_test.dart
+++ test/paper_icon_button_basic_test.dart
@@ -3,6 +3,8 @@ library polymer_elements.test.paper_icon_button_basic_test;
 
 import 'package:polymer_elements/paper_icon_button.dart';
 import 'package:polymer_elements/iron_icon.dart';
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_interop/polymer_interop.dart';
 import 'package:test/test.dart';
 import 'package:web_components/web_components.dart';
 import 'common.dart';
@@ -21,9 +23,8 @@ main() async {
 
     test('applies an icon specified by the `icon` attribute', () {
       expect(b1.src, isNull);
-      expect(b1.querySelector('svg'),
-          isNotNull);
-    }, skip: 'https://github.com/dart-lang/polymer_elements/issues/31');
+      expect(Polymer.dom(b1.$['icon'].root).querySelector('svg'), isNotNull);
+    });
 
     test('applies an icon specified by the `src` attribute', () {
       expect(b2.src, isNotNull);
