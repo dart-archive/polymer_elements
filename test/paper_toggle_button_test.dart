7e75c0d165f862b2c99575ec51f6c2dd8a821aea
Victor Sheyanov <victor.sheyanov@gmail.com>
2015-08-20 15:02:59 +0300

    another batch of paper elements tests

---
+++ test/paper_toggle_button_test.dart
@@ -0,0 +1,83 @@
+@TestOn('browser')
+library polymer_elements.test.paper_toggle_button_test;
+
+import 'dart:async';
+import 'package:polymer_elements/paper_toggle_button.dart';
+import 'package:web_components/web_components.dart';
+import 'package:test/test.dart';
+import 'common.dart';
+
+/**
+ * Original tests:
+ * https://github.com/PolymerElements/paper-toggle-button/tree/master/test
+ */
+
+main() async {
+    await initWebComponents();
+
+    group('defaults', () {
+        PaperToggleButton b1;
+
+        setUp(() {
+            b1 = fixture('basic');
+        });
+
+        test('check button via click', () {
+            Completer done = new Completer();
+
+            b1.on['click'].take(1).listen((_){
+                expect(b1.getAttribute('aria-checked'), equals(isTrue));
+                expect(b1.checked, isTrue);
+                done.complete();
+            });
+
+            tap(b1);
+
+            return done.future;
+        }, skip: 'https://github.com/dart-lang/polymer_elements/issues/47');
+
+        test('toggle button via click', () {
+            Completer done = new Completer();
+
+            b1.checked = true;
+
+            b1.on['click'].take(1).listen((_){
+                expect(b1.getAttribute('aria-checked'), equals(isFalse));
+                expect(b1.checked, isFalse);
+                done.complete();
+            });
+
+            tap(b1);
+
+            return done.future;
+        }, skip: 'https://github.com/dart-lang/polymer_elements/issues/47');
+
+        test('disabled button cannot be clicked', () {
+            Completer done = new Completer();
+
+            b1.disabled = true;
+
+            b1.on['click'].take(1).listen((_){
+                expect(b1.getAttribute('aria-checked'), equals(isTrue));
+                expect(b1.checked, isTrue);
+                done.complete();
+            });
+
+            tap(b1);
+
+            return done.future;
+        }, skip: 'https://github.com/dart-lang/polymer_elements/issues/47');
+    });
+
+    group('a11y', () {
+        PaperToggleButton b1;
+
+        setUp(() {
+            b1 = fixture('basic');
+        });
+
+        test('has aria role "button"', () {
+            expect(b1.getAttribute('role'), equals('button'));
+        });
+    });
+}
---
+++ test/paper_toolbar_test.dart
@@ -0,0 +1,102 @@
+@TestOn('browser')
+library polymer_elements.test.paper_toolbar_test;
+
+import 'dart:html';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'package:web_components/web_components.dart';
+import 'package:test/test.dart';
+import 'common.dart';
+
+/**
+ * Original tests:
+ * https://github.com/PolymerElements/paper-toolbar/tree/master/test
+ */
+
+main() async {
+    await initWebComponents();
+
+    group('basic', () {
+    
+        PaperToolbar toolbar;
+
+        setUp(() {
+            toolbar = fixture('basic');
+        });
+
+        test('has expected medium-tall height', () {
+            int old = toolbar.offsetHeight;
+            toolbar.classes.add('medium-tall');
+            expect(toolbar.offsetHeight, equals(old * 2));
+        });
+
+        test('has expected tall height', () {
+            int old = toolbar.offsetHeight;
+            toolbar.classes.add('tall');
+            expect(toolbar.offsetHeight, equals(old * 3));
+        });
+
+        test('distributes nodes to topBar by default', () {
+            DivElement item = new DivElement();
+            Polymer.dom(toolbar).append(item);
+            PolymerDom.flush();
+
+            HtmlElement insertionPoint = Polymer.dom(item).getDestinationInsertionPoints()[0];
+            expect(Polymer.dom(insertionPoint).parentNode, equals(toolbar.$['topBar']));
+        });
+
+        test('distributes nodes with "middle" class to middleBar', () {
+            DivElement item = new DivElement();
+            item.classes.add('middle');
+            Polymer.dom(toolbar).append(item);
+            PolymerDom.flush();
+
+            HtmlElement insertionPoint = Polymer.dom(item).getDestinationInsertionPoints()[0];
+            expect(Polymer.dom(insertionPoint).parentNode, equals(toolbar.$['middleBar']));
+        });
+
+        test('distributes nodes with "bottom" class to bottombar', () {
+            DivElement item = new DivElement();
+            item.classes.add('bottom');
+            Polymer.dom(toolbar).append(item);
+            PolymerDom.flush();
+
+            HtmlElement insertionPoint = Polymer.dom(item).getDestinationInsertionPoints()[0];
+            expect(Polymer.dom(insertionPoint).parentNode, equals(toolbar.$['bottomBar']));
+        });
+    });
+
+    group('a11y', () {
+
+        test('has role="toolbar"', () {
+            PaperToolbar toolbar = fixture('basic');
+            expect(toolbar.getAttribute('role'), equals('toolbar'), reason: 'should have role="toolbar"');
+        });
+
+        test('children with .title becomes the label', () {
+            PaperToolbar toolbar = fixture('title');
+            expect(toolbar.getAttribute('aria-labelledby'), isNotNull, reason: 'should have aria-labelledby');
+            expect(toolbar.getAttribute('aria-labelledby'), equals(toolbar.querySelector('.title').id), reason: 'aria-labelledby should have the id of the .title element');
+        });
+
+        test('existing ids on titles are preserved', () {
+            PaperToolbar toolbar = fixture('title-with-id');
+            expect(toolbar.getAttribute('aria-labelledby'), isNotNull, reason: 'should have aria-labelledby');
+            expect(toolbar.querySelector('.title').id, equals('title'), reason: 'id should be preserved');
+        });
+
+        test('multiple children with .title becomes the label', () {
+            PaperToolbar toolbar = fixture('multiple-titles');
+
+            expect(toolbar.getAttribute('aria-labelledby'), isNotNull, reason: 'should have aria-labelledby');
+
+            List ids = [];
+            List titles = toolbar.querySelectorAll('.title');
+            titles.forEach((title){
+                ids.add(title.id);
+            });
+
+            expect(toolbar.getAttribute('aria-labelledby'), equals(ids.join(' ')), reason: 'aria-labelledby should have the id of all .title elements');
+        });
+    });
+}
