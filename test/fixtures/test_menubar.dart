b0c0ab962dc9a6e5cda8fee958824dc3a28b3f19
Jacob MacDonald <jakemac@google.com>
2015-08-21 12:09:49 -0700

    add iron-menubar-behavior tests

--- test/fixtures/test_menubar.dart
+++ test/fixtures/test_menubar.dart
@@ -2,11 +2,21 @@
 library polymer_elements.test.fixtures.test_menubar;
 
 import 'package:polymer/polymer.dart';
+import 'package:polymer_elements/iron_a11y_keys_behavior.dart';
+import 'package:polymer_elements/iron_menu_behavior.dart';
 import 'package:polymer_elements/iron_menubar_behavior.dart';
+import 'package:polymer_elements/iron_multi_selectable.dart';
+import 'package:polymer_elements/iron_selectable.dart';
 import 'package:web_components/web_components.dart';
 
 @jsProxyReflectable
 @PolymerRegister('test-menubar')
-class TestMenu extends PolymerElement with IronMenubarBehavior {
-  TestMenu.created() : super.created();
+class TestMenuBar extends PolymerElement
+    with
+        IronMenuBehavior,
+        IronMultiSelectableBehavior,
+        IronSelectableBehavior,
+        IronA11yKeysBehavior,
+        IronMenubarBehavior {
+  TestMenuBar.created() : super.created();
 }
