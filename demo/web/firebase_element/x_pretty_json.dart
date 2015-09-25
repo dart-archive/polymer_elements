c983eed72115a1b160ade48b4e808d7481351d54
zoechi <gzoechi@gmail.com>
2015-09-25 21:01:17 +0200

    - add gold_cc_expiration_input demo - add gold_cc_input demo - add styles/demo_elements to make top-level styles work with demos wrapped in `<app-element>` - add license text in all files - firebase-element   - replace `dom-bind` with properties in app-element   - add missing type annotation   - use `set('message', ...)` - add missing entries to `.gitignore`

--- demo/web/firebase_element/x_pretty_json.dart
+++ demo/web/firebase_element/x_pretty_json.dart
@@ -1,3 +1,12 @@
+/*
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
 @HtmlImport('x_pretty_json.html')
 library x_pretty_json;
 
@@ -11,7 +20,7 @@ class XPrettyJson extends PolymerElement {
   @property Map object;
 
   @eventHandler
-  String prettify(Map object) {
+  String prettify(Object object) {
     if (object == null) {
       return '';
     }
