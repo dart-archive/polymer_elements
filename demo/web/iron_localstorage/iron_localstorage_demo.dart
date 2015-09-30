927b6658bed5ac82686758aefe300dd337c0c647
zoechi <gzoechi@gmail.com>
2015-09-30 18:19:51 +0200

    - fix missing mixin in iron_behaviors - add iron_localstorage demo - add iron_media_query demo - add iron_menu_behavior demo

---
+++ demo/web/iron_localstorage/iron_localstorage_demo.dart
@@ -0,0 +1,40 @@
+/*
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+@HtmlImport('iron_localstorage_demo.html')
+library polymer_elements_demo.web.iron_localstorage.iron_localstorage_demo;
+
+import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer/polymer.dart';
+import 'package:polymer_elements/paper_styles.dart';
+import 'package:polymer_elements/paper_checkbox.dart';
+import 'package:polymer_elements/iron_localstorage.dart';
+import 'package:polymer_elements_demo/styles/demo_elements.dart';
+
+/// Silence analyzer [PaperStyles], [PaperCheckbox], [IronLocalstorage], [DemoElements],
+@PolymerRegister('iron-localstorage-demo')
+class IronLocalstorageDemo extends PolymerElement {
+  IronLocalstorageDemo.created() : super.created();
+
+  Map _value;
+  Map get value => _value;
+  set value(var value) {
+    if(value is Map) {
+      _value = value;
+    } else {
+      _value = convertToDart(value);
+    }
+  }
+
+  @eventHandler
+  void initializeDefaultValue([_, __]) {
+    print("initializeTemplate");
+    set('value', convertToJs({'name': "Mickey", 'hasEars': true}));
+  }
+}
--- demo/web/iron_menu_behavior/simple_menu.dart
+++ demo/web/iron_menu_behavior/simple_menu.dart
@@ -7,20 +7,24 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
-@HtmlImport('simple_button.html')
-library polymer_elements_demo.web.web.iron_behaviors.simple_button;
+@HtmlImport('simple_menu.html')
+library polymer_elements_demo.web.web.iron_menu_behavior.simple_menu;
 
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer/polymer.dart';
-import 'package:polymer_elements/iron_control_state.dart';
-import 'package:polymer_elements/iron_button_state.dart';
+import 'package:polymer_elements/iron_menu_behavior.dart';
 import 'package:polymer_elements/iron_a11y_keys_behavior.dart';
+import 'package:polymer_elements/iron_multi_selectable.dart';
+import 'package:polymer_elements/iron_selectable.dart';
 
 /// Silence analyzer
-@PolymerRegister('simple-button')
-class SimpleButton extends PolymerElement
-    with IronControlState, IronA11yKeysBehavior, IronButtonState {
-  SimpleButton.created() : super.created();
-
-  static const hostAttributes = const {'role': 'button',};
+@PolymerRegister('simple-menu')
+class SimpleMenu extends PolymerElement
+    with
+        IronSelectableBehavior,
+        IronMultiSelectableBehavior,
+        IronA11yKeysBehavior,
+        IronMenuBehavior
+  {
+  SimpleMenu.created() : super.created();
 }
