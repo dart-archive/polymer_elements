2190450ad2c99181818634c81061ef02bbdffb34
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2015-12-16 00:57:30 +0100

    missing helpers and mock restored

--- lib/mock_interactions.dart
+++ lib/mock_interactions.dart
@@ -1,1 +1,10 @@
-library mock.interactions;
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `mock_interactions`.
+@HtmlImport('mock_interactions_nodart.html')
+library polymer_elements.lib.src.iron_test_helpers.mock_interactions;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
