7d6f930789dfaaf9395fc1708ed7f48308e0e15c
Jacob MacDonald <jakemac@google.com>
2016-01-08 11:12:59 -0800

    add iron_doc_property_styles and iron_doc_viewer_styles

---
+++ lib/iron_doc_viewer_styles.dart
@@ -0,0 +1,11 @@
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `iron_doc_viewer_styles`.
+@HtmlImport('iron_doc_viewer_styles_nodart.html')
+library polymer_elements.lib.src.iron_doc_viewer.iron_doc_viewer_styles;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'typography.dart';
